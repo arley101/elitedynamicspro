5aded0fdf0e5ae910f0ea01872bbcea14e628dfd
arley101 <ceo@elitecosmeticdental.com>
2025-05-01 07:49:33 -0500

    Update constants.py

--- Shared/constants.py
+++ Shared/constants.py
@@ -1,9 +1,5 @@
+# shared/constants.py
+
 # Constantes globales compartidas
 BASE_URL = "https://graph.microsoft.com/v1.0"
 GRAPH_API_TIMEOUT = 45
-# Puedes añadir otras constantes aquí si las necesitas en varios sitios
-
-# Constantes específicas para otras APIs (si se usan en varios módulos)
-# Ejemplo (aunque PA y PBI las definen localmente ahora):
-# AZURE_MGMT_BASE_URL = "https://management.azure.com"
-# PBI_BASE_URL = "https://api.powerbi.com/v1.0/myorg"
